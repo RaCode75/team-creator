76fa7569ea710e8e9d3caa9ea1288588a18eeb29
RaCode75 <racode75@gmail.com>
2022-09-01 18:22:44 -0300

    all variables and some logic added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,27 @@ export class AppComponent {
   newMemberName: string = '';
   members: string[] = [];
   errorMessage: string = '';
+  numOfTeams: number | '' = '' ;
+  teams: string[][] = [];
 
   onInput(member: string){
     this.newMemberName = member;
+  }
 
+  onNumOfTeamsInput(value: string){
+    this.numOfTeams = Number(value);
+  }
+
+  generateTeams(){
+    if(!this.numOfTeams || this.numOfTeams <=0){
+      return;
+    }
+    const allMembers = [...this.members];
+
+    for(let i =0; i<this.numOfTeams; i++){
+      const randomIndex = Math.floor(Math.random() * allMembers.length);
+      const member = allMembers.slice(randomIndex, 1) [0];
+    }
   }
 
 
