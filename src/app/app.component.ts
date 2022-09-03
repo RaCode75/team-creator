43bad89e631273b46863db037bfefb17ef27c4a8
RaCode75 <racode75@gmail.com>
2022-09-03 05:55:49 -0300

    Logi complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TitleStrategy } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -20,19 +21,6 @@ export class AppComponent {
     this.numOfTeams = Number(value);
   }
 
-  generateTeams(){
-    if(!this.numOfTeams || this.numOfTeams <=0){
-      return;
-    }
-    const allMembers = [...this.members];
-
-    for(let i =0; i<this.numOfTeams; i++){
-      const randomIndex = Math.floor(Math.random() * allMembers.length);
-      const member = allMembers.slice(randomIndex, 1) [0];
-    }
-  }
-
-
   addMember(){
     if(!this.newMemberName){
       this.errorMessage = "Name not be empty";
@@ -41,6 +29,37 @@ export class AppComponent {
     this.errorMessage = "";
     this.members.push(this.newMemberName);
     this.newMemberName='';
+  }
 
+  generateTeams(){
+
+    // error handler
+    if(!this.numOfTeams || this.numOfTeams <=0){
+      this.errorMessage = "Invalid input"
+      return;
+    }
+
+    if(this.members.length < this.numOfTeams){
+      this.errorMessage = "Not enought members";
+      return;
+    }
+
+    this.errorMessage = "";
+    const allMembers = [...this.members];
+
+while(allMembers.length){
+  for(let i =0; i < this.numOfTeams; i++){
+    const randomIndex = Math.floor(Math.random() * allMembers.length);
+    const member = allMembers.splice(randomIndex, 1) [0];
+
+    if(this.teams[i]){
+      this.teams[i].push(member);
+      } else {
+        this.teams[i] = [member];
+        }
+      }
+    }
+    this.members = [];
+    this.numOfTeams = "";
   }
 }
